487822d731845ae36a383b7eb6a9e147e813ffb5
Sebastian Knell <sebastianknell@hotmail.com>
2023-03-14 11:14:51 -0500

    updated data

--- src/data.ts
+++ src/data.ts
@@ -2,7 +2,11 @@ export const saleTypes = ["alquiler", "venta"];
 
 export const propertyTypes = ["departamentos", "casas"];
 
-export const districts = [
+type OptionType = {
+  value: string;
+  label: string;
+}
+export const districts: OptionType[] = [
   { value: "san-borja", label: "San Borja" },
   { value: "san-isidro", label: "San Isidro" },
   { value: "santiago-de-surco", label: "Santiago de Surco" },
@@ -20,4 +24,30 @@ export const districts = [
   { value: "chorrillos", label: "Chorrillos" },
   { value: "villa-el-salvador", label: "Villa El Salvador" },
   { value: "san-juan-de-miraflores", label: "San Juan de Miraflores" },
+  { value: "lima", label: "Lima"},
+  { value: "ancon", label: "Ancón"},
+  { value: "breña", label: "Breña"},
+  { value: "carabayllo", label: "Carabayllo"},
+  { value: "chaclacayo", label: "Chaclacayo"},
+  { value: "cieneguilla", label: "Cieneguilla"},
+  { value: "comas", label: "Comas"},
+  { value: "el-agustino", label: "El Agustino"},
+  { value: "independencia", label: "Independencia"},
+  { value: "los-olivos", label: "Los Olivos"},
+  { value: "lurigancho", label: "Lurigancho"},
+  { value: "lurin", label: "Lurín"},
+  { value: "pachacamac", label: "Pachacámac"},
+  { value: "pucusana", label: "Pucusana"},
+  { value: "pueblo-libre", label: "Pueblo Libre"},
+  { value: "puente-piedra", label: "Puente Piedra"},
+  { value: "punta-hermosa", label: "Punta Hermosa"},
+  { value: "punta-negra", label: "Punta Negra"},
+  { value: "rimac", label: "Rímac"},
+  { value: "san-bartolo", label: "San Bartolo"},
+  { value: "san-juan-de-lurigancho", label: "San Juan de Lurigancho"},
+  { value: "san-luis", label: "San Luis"},
+  { value: "san-martin-de-porres", label: "San Martín de Porres"},
+  { value: "santa-maria-del-mar", label: "Santa María del Mar"},
+  { value: "santa-rosa", label: "Santa Rosa"},
+  { value: "villa-maria-del-triunfo", label: "Villa María del Triunfo"},
 ];
